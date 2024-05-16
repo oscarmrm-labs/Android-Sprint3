c4acf7aa7c7a6e284de4c20103e21d09ef5697d7
OMRM <oscarmrm14@gmail.com>
2024-05-16 21:27:30 +0200

    Add Screen detail

---
+++ app/src/main/java/com/qualentum/sprint3/DetailDay.kt
@@ -0,0 +1,29 @@
+package com.qualentum.sprint3
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailDay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_day)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        showIndex()
+    }
+
+    private fun showIndex() {
+        val b = intent.getStringExtra("dayInfo")
+        val prueba = findViewById<TextView>(R.id.textView12)
+        prueba.text = b
+    }
+
+}
