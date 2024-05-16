45e1381df8311944f1a52225826a7eb7314672e0
OMRM <oscarmrm14@gmail.com>
2024-05-16 21:25:34 +0200

    Add days recyclerView

---
+++ app/src/main/java/com/qualentum/sprint3/list/DayInfo.kt
@@ -0,0 +1,9 @@
+package com.qualentum.sprint3.list
+
+class DayInfo(
+    val sunrise: String,
+    val sunset: String,
+    val temperatureMax: String,
+    val temperatureMin: String,
+    val time: String
+)
