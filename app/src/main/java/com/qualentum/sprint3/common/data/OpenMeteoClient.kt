25768e75b16643a281ff19ddd27d35ae93cd2cc2
OMRM <oscarmrm14@gmail.com>
2024-05-21 15:05:05 +0200

    Improve retrofitClient adding: certificatePinner, hostnameVerifier and timeouts

--- app/src/main/java/com/qualentum/sprint3/common/data/OpenMeteoClient.kt
+++ app/src/main/java/com/qualentum/sprint3/common/data/OpenMeteoClient.kt
@@ -1,19 +1,73 @@
 package com.qualentum.sprint3.common.data
 
+import android.util.Log
 import com.qualentum.sprint3.detail.data.repository.DetailMeteoAPIService
 import com.qualentum.sprint3.main.data.repository.MainMeteoAPIService
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.HostnameVerifier
 
 object OpenMeteoClient {
-/*
+
+    private const val RETRFIT_TIMEOUT_IN_SECOND = 5L
+    private const val SHA256_OPEN_METEO = "FUyUZOkodJVEZGiT9iMn76FV0e37t9rsS2zvz55roBk="
+    private val retrofit: Retrofit
+    val mainService: MainMeteoAPIService
+    val detailService: DetailMeteoAPIService
+    init {
+        Log.i("TAG", "init retrofit: ")
+        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add("api.open-meteo.com", "sha256/$SHA256_OPEN_METEO")
+            .build()
+        httpClient.certificatePinner(certificatePinner)
+
+        val hostNamesAllow = listOf(
+            "api.open-meteo.com"
+        )
+
+        val hostNameVerifier = HostnameVerifier { hostname, session ->
+            hostname in hostNamesAllow
+        }
+        httpClient.hostnameVerifier(hostNameVerifier)
+
+        httpClient
+            .connectTimeout(RETRFIT_TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
+            .readTimeout(RETRFIT_TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
+            .writeTimeout(RETRFIT_TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
+
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://api.open-meteo.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build()
+
+        mainService = retrofit.create(
+            MainMeteoAPIService::class.java
+        )
+
+        detailService = retrofit.create(
+            DetailMeteoAPIService::class.java
+        )
+
+        Log.i("TAG", "finish retrofit: ")
+    }
+    /*
     val serverHostName = "https://open-meteo.com"
 
     // TODO: Ver como obtener certificado de la página
     val certificatePinner = CertificatePinner.Builder()
         .add(serverHostName, "")
         .build()
-*/
+    httpClient
+
+    val hostNamesAllow = listOf(
+        "open-meteo.com"
+    )
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://api.open-meteo.com/v1/")
@@ -27,4 +81,6 @@ object OpenMeteoClient {
     val detailService = retrofit.create(
         DetailMeteoAPIService::class.java
     )
+
+     */
 }
